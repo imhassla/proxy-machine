8e1f14fc78792013b583aac3a7971e09c12728b5
Daddy_0x <45751261+imhassla@users.noreply.github.com>
2023-08-17 23:57:20 +0700

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -19,10 +19,10 @@ from socks import set_default_proxy, SOCKS4, SOCKS5, HTTP, socksocket
 
 # Set up command line argument parsing
 parser = argparse.ArgumentParser(description='The script retrieves and checks http, https, socks4 and socks5 proxies')
-parser.add_argument('-l', type=int, default=70, help='limit of proxies stored in last_checked.txt')
-parser.add_argument('-p', type=int, default=1500, help='ping (ms.) of the proxy server. (for default providers only)')
+parser.add_argument('-l', type=int, default=50, help='limit of proxies stored in last_checked.txt')
+parser.add_argument('-p', type=int, default=3500, help='ping (ms.) of the proxy server. (for default providers only)')
 parser.add_argument('-t', type=int, default=5, help='timeout (s.) of checker')
-parser.add_argument('-w', type=int, default=25, help='number of worker threads to use when checking proxies')
+parser.add_argument('-w', type=int, default=50, help='number of worker threads to use when checking proxies')
 parser.add_argument('-type', type=str, default='socks4', choices=['http', 'https', 'socks4', 'socks5'], help='type of proxies to retrieve and check')
 parser.add_argument('-top', action='store_true', help='If specified, store top 10 proxies in file')
 parser.add_argument('-scan', action='store_true', help='If specified, perform scan.py for checked proxies ip ranges.')
@@ -38,7 +38,7 @@ logging.basicConfig(level=logging.INFO)
 os.system('cls' if os.name == 'nt' else 'clear')
 
 # Set the maximum number of open file descriptors to 4000
-os.system('ulimit -n 4000')
+os.system('ulimit -n 40000')
 
 
 # Set the API URL for retrieving proxies based on the command line arguments or use the default URL if not specified
@@ -47,13 +47,13 @@ if args.url:
 else:
     api_url = f"https://api.proxyscrape.com/v2/?request=displayproxies&protocol={args.type}&timeout={args.p}&country=all&ssl=all&anonymity=all"
 
-# Set the maximum number of proxies to store in memory and other variables based on the command line arguments
-max = args.l*10 
 t = args.t
 workers = args.w
 
 # Initialize a set to store the proxies and other variables for tracking proxy statistics and availability
 proxies = set()
+alive_proxies_set = set()
+alive_proxies_lock = threading.Lock()
 checked_filename = "last_checked.txt"
 top10_filename = "top10.txt"
 file_lock = Lock()
@@ -159,9 +159,7 @@ def get_proxies():
     # Reset the default proxy settings.
     socks.set_default_proxy()
     # If the number of proxies in memory has reached the maximum limit, clear the set of proxies.
-    if len(proxies) >= max:
-        with file_lock:
-            proxies.clear()
+
     # If the number of checked proxies in the last_checked.txt file has reached the limit specified by the command line arguments, return without retrieving any new proxies.
     with open(checked_filename, 'r') as f:
         if len(f.readlines()) > args.l:
@@ -186,7 +184,7 @@ def get_proxies():
     for source in additional_sources:
         try:
             response = requests.get(source)
-            new_proxies = set(response.text.splitlines()[:args.l*3])
+            new_proxies = set(response.text.splitlines())
             with file_lock:
                 # Update the set of proxies in memory with any new proxies that were not already in the set.
                 proxies.update(new_proxies - proxies)
@@ -195,17 +193,15 @@ def get_proxies():
             pass
 
 # Define a function to check all of the proxies in memory and in the last_checked.txt file using multiple worker threads.
+
 def check_proxies():
 
-    # Read the last_checked.txt file and combine its contents with the set of proxies in memory to create a set of all known proxies.
-    with open(checked_filename, "r") as f:
-        checked_proxies = set(f.read().splitlines())
-        all_proxies = proxies | checked_proxies
+    # Combine the set of proxies in memory with the set of alive proxies to create a set of all known proxies.
+    all_proxies = proxies 
 
     # Use a ThreadPoolExecutor to check all of the known proxies concurrently using multiple worker threads.
     with ThreadPoolExecutor(max_workers=workers) as executor:
         futures = {}
-        results = []
         for proxy in all_proxies:
             future = executor.submit(check_proxy, proxy, proxy_type)
             futures[future] = proxy
@@ -215,31 +211,58 @@ def check_proxies():
             if result is not None:
                 # Unpack the result tuple into separate variables
                 proxy, rounded_resp_time, current_time = result
-                # Append the result values to the results list
-                results.append((proxy, rounded_resp_time, current_time))
+                
+                # Update the set of alive proxies in memory.
+                with alive_proxies_lock:
+                    alive_proxies_set.add(proxy)
+                
+                if args.db:
+                    conn = sqlite3.connect('data.db',timeout = 10)
+                    c = conn.cursor()
+                    c.execute(f'''CREATE TABLE IF NOT EXISTS {proxy_type} (proxy TEXT PRIMARY KEY, response_time REAL, last_checked TEXT)''')
+                    c.execute('BEGIN')
+                    c.execute(f'''INSERT OR REPLACE INTO {proxy_type} (proxy, response_time, last_checked) VALUES (?, ?, ?)''', (proxy, rounded_resp_time, current_time))
+                    c.execute('COMMIT')
+                    conn.close()
             else:
                 proxy = futures[future]
                 # If a proxy check was not successful, remove it from the set of proxies in memory.
                 proxies.discard(proxy)
-    if results:
-        if args.db:
-            conn = sqlite3.connect('data.db',timeout = 10)
-            c = conn.cursor()
-            c.execute(f'''CREATE TABLE IF NOT EXISTS {proxy_type} (proxy TEXT PRIMARY KEY, response_time REAL, last_checked TEXT)''')
-            c.execute('BEGIN')
-            c.executemany(f'''INSERT OR REPLACE INTO {proxy_type} (proxy, response_time, last_checked) VALUES (?, ?, ?)''', results)
-            c.execute('COMMIT')
-            conn.close()
-        
-    # Sort the list of alive proxies by response time and extract only the proxy strings from each tuple.
-    alive_proxies = sorted(results, key=lambda x: x[1])
-    alive_proxies = [proxy[0] for proxy in alive_proxies]
+                
 
-    # Write the list of alive proxies to the last_checked.txt file.
+
+def recheck_alive_proxies():
+
+    while True:
+        # Create a copy of the set of alive proxies
+        alive_proxies = set(alive_proxies_set)
+
+        # Use a ThreadPoolExecutor to check all of the alive proxies concurrently using multiple worker threads.
+        with ThreadPoolExecutor(max_workers=workers) as executor:
+            futures = {}
+            for proxy in alive_proxies:
+                future = executor.submit(check_proxy, proxy, proxy_type)
+                futures[future] = proxy
+            
+            for future in as_completed(futures):
+                result = future.result()
+                if result is None:
+                    proxy = futures[future]
+                    # If a proxy check was not successful, remove it from the set of alive proxies in memory.
+                    with alive_proxies_lock:
+                        alive_proxies_set.discard(proxy)
+        
+        # Sleep for 10 seconds before rechecking the proxies again.
+        time.sleep(10)
+
+def write_alive_proxies_to_file():
+       
+    # Write the list of alive proxies to the checked_filename file.
     with file_lock, open(checked_filename, "w") as f:
-        for proxy in alive_proxies:
+        for proxy in alive_proxies_set:
             f.write(proxy + "\n")
-        results = []
+    
+
 
 # Define a function to track the availability of checked proxies over time and display statistics about their uptime.
 def track_proxies():
@@ -370,23 +393,34 @@ def scan_ip_ranges():
                 # Get the updated list of IP ranges
                 ip_ranges, ports = get_ip_ranges()
 
-
 if args.scan:
         scan_thread = threading.Thread(target=scan_ip_ranges)
         scan_thread.start()
 
+def run_thread(func, interval):
+    # Run the given function at the specified interval
+    while True:
+        func()
+        time.sleep(interval)
 
-# Define the main function to run when the script is executed.
 if __name__ == "__main__":
-    # Schedule the get_proxies, check_proxies, and track_proxies functions to run at regular intervals using the schedule library.
-    schedule.every(14).seconds.do(get_proxies)
-    schedule.every(20).seconds.do(check_proxies)
-    schedule.every(10).seconds.do(track_proxies)
+    # Create and start threads for each of the functions
+    t1 = threading.Thread(target=run_thread, args=(get_proxies, 15))
+    t2 = threading.Thread(target=run_thread, args=(check_proxies, 10))
+    t3 = threading.Thread(target=run_thread, args=(track_proxies, 10))
+    t4 = threading.Thread(target=run_thread, args=(write_alive_proxies_to_file, 2))
+    t5 = threading.Thread(target=recheck_alive_proxies)
     
-    try:
-        # Continuously run scheduled tasks until interrupted by a keyboard interrupt (Ctrl-C).
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
-    except KeyboardInterrupt:
-        logging.info('Exiting...')
+    
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    
+    # Wait for all threads to finish
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
