a1bf8d5537cefd6dfd995cec5158182414a015db
Daddy_0x <45751261+imhassla@users.noreply.github.com>
2024-11-10 21:33:49 +0700

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -22,17 +22,17 @@ from urllib3.exceptions import ProxyError, SSLError, ConnectTimeoutError, ReadTi
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# Set up command line argument parsing
-parser = argparse.ArgumentParser(description='The script retrieves and checks http, https, socks4 and socks5 proxies')
-parser.add_argument('-p', type=int, default=5000, help='ping (ms.) of the proxy server. (for default providers only)')
-parser.add_argument('-t', type=int, default=5, help='timeout (s.) of checker')
-parser.add_argument('-w', type=int, default=200, help='number of worker threads to use when checking proxies')
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Script for retrieving and checking http, https, socks4, and socks5 proxies')
+parser.add_argument('-p', type=int, default=5000, help='ping (ms) of the proxy server (default providers only)')
+parser.add_argument('-t', type=int, default=5, help='timeout (s) for checker')
+parser.add_argument('-w', type=int, default=200, help='number of worker threads for proxy checking')
 parser.add_argument('-type', type=str, default='socks4', choices=['http', 'https', 'socks4', 'socks5'], help='type of proxies to retrieve and check')
-parser.add_argument('-api', action='store_true', help='If specified, dont track proxies ')
-parser.add_argument('-scan', action='store_true', help='If specified, perform scan.py for checked proxies ip ranges.')
-parser.add_argument('-sw', type=int, default=3, help='number of scanner workers threads')
-parser.add_argument('-db', action='store_true', help='store checked proxies in db')
-parser.add_argument('-url', type=str, help='"URL" of the API to retrieve proxies from')
+parser.add_argument('-api', action='store_true', help='skip tracking proxies if specified')
+parser.add_argument('-scan', action='store_true', help='run scan.py for checked proxy IP ranges if specified')
+parser.add_argument('-sw', type=int, default=3, help='number of scanner worker threads')
+parser.add_argument('-db', action='store_true', help='store checked proxies in database')
+parser.add_argument('-url', type=str, help='URL of the API to retrieve proxies from')
 args = parser.parse_args()
 
 # Clear the screen
@@ -72,104 +72,67 @@ while True:
         print(f' Connection error: {e}. Retrying in 5 seconds...',end="\r")
         time.sleep(5)
 
+# Function to check proxies
 def check_proxy(proxy, proxy_type):
-    # Record the current time in the format YYYY-MM-DD HH:MM:SS
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
     try:
-        # Split the proxy string into host and port
         proxy_host, proxy_port = proxy.split(':')
-        # Define the URL for checking the proxy
         url = 'https://httpbin.org/ip'
         
-        # Check if the proxy type is 'http' or 'https'
         if proxy_type in ['http', 'https']:
-            # Create a ProxyManager instance for the given proxy
             http = urllib3.ProxyManager(
                 f"{proxy_type}://{proxy_host}:{proxy_port}",
-                timeout=urllib3.Timeout(connect=args.t, read=args.t),  # Set connection and read timeouts
-                retries=False,  # Disable retries in urllib3
-                cert_reqs='CERT_NONE',  # Do not verify SSL certificates
-                assert_hostname=False  # Do not verify hostname in SSL certificate
+                timeout=urllib3.Timeout(connect=args.t, read=args.t),
+                retries=False,
+                cert_reqs='CERT_NONE',
+                assert_hostname=False
             )
-            
-            # Record the start time for measuring response time
             start_time = time.time()
-            # Make a GET request through the proxy
             response = http.request('GET', url, preload_content=False)
-            # Record the end time for measuring response time
-            end_time = time.time()
-            # Calculate the response time
-            response_time = end_time - start_time
-            # Decode the response data from bytes to string and parse it as JSON
+            response_time = time.time() - start_time
             data = json.loads(response.data.decode('utf-8'))
             
-            # Check if the IP address returned by the proxy matches the user's IP address
             if any(origin == sip for origin in data.get('origin').split(', ')):
-                return None  # If the IP matches, the proxy is valid but no need to return any data
+                return None
             else:
-                # If the IP does not match, return a tuple with the proxy details and response time
-                return (f'{proxy_host}:{proxy_port}', response_time, current_time)
+                return f'{proxy_host}:{proxy_port}', response_time, current_time
         
-        # Check if the proxy type is 'socks4' or 'socks5'
         elif proxy_type in ['socks4', 'socks5']:
-            # Configure the SOCKS proxy based on the type
-            if proxy_type == 'socks4':
-                socks.set_default_proxy(socks.SOCKS4, proxy_host, int(proxy_port))
-                socket.socket = socks.socksocket
-            elif proxy_type == 'socks5':
-                socks.set_default_proxy(socks.SOCKS5, proxy_host, int(proxy_port))
-                socket.socket = socks.socksocket
-            
-            # Make a GET request through the SOCKS proxy
-            # Headers including 'X-Forwarded-For'
-            headers = {
-                'X-Forwarded-For': proxy_host  # This will explicitly set the proxy forwarding IP
-            }
-
+            socks.set_default_proxy(SOCKS4 if proxy_type == 'socks4' else SOCKS5, proxy_host, int(proxy_port))
+            socket.socket = socks.socksocket
+            headers = {'X-Forwarded-For': proxy_host}
             r = requests.get(url, timeout=args.t, verify=False, headers=headers)
             
-            # Check if the response status code is 200 (OK)
             if r.status_code == 200:
-                # Calculate the response time
                 response_time = r.elapsed.total_seconds()
-                # Parse the response as JSON
                 data = r.json()
                 
-                # Check if the IP address returned by the proxy matches the user's IP address
                 if any(origin == sip for origin in data.get('origin').split(', ')):
-                    return None  # If the IP matches, the proxy is valid but no need to return any data
+                    return None
                 else:
-                    # If the IP does not match, return a tuple with the proxy details and response time
-                    return (f'{proxy_host}:{proxy_port}', response_time, current_time)
+                    return f'{proxy_host}:{proxy_port}', response_time, current_time
     
     except (Exception, SSLError, ProxyError, ConnectTimeoutError, ReadTimeoutError) as e:
-        # Handle exceptions related to the proxy check
-        # Uncomment the line below to print the error message for debugging
-        # print(f"General Error for proxy {proxy}: {e}")
-        return None  # Return None if any exception occurs
+        return None
     
     finally:
-        # Reset the SOCKS proxy to the default state
         socks.set_default_proxy()
-        # Reset the socket to the default socket
         socket.socket = socket.socket
     
-    # Return None if no valid response was obtained
     return None
 
+# Function to retrieve proxies from API and additional sources
 def get_proxies():
     try:
-        # Reset the default proxy settings.
         socks.set_default_proxy()
-        # Try to retrieve proxies from the API URL specified by the command line arguments or the default API URL.
         try:
             response = requests.get(api_url)
             new_proxies = set(response.text.splitlines())
             proxies.update(new_proxies - proxies)
         except:
             pass
-        # Define additional sources for retrieving proxies.
+
         additional_sources = [
             f"https://www.proxy-list.download/api/v1/get?type={proxy_type}",
             f"https://github.com/mmpx12/proxy-list/blob/master/{proxy_type}.txt",
@@ -180,7 +143,6 @@ def get_proxies():
 
         ip_port_pattern = re.compile(r'^\d{1,3}(\.\d{1,3}){3}:\d+$')
 
-        # Try to retrieve proxies from each additional source.
         for source in additional_sources:
             try:
                 response = requests.get(source)
@@ -191,10 +153,11 @@ def get_proxies():
     except:
         pass
 
+# Function to save proxy details to the queue
 def save_proxy_to_queue(proxy, rounded_resp_time, current_time):
     db_queue.put((proxy, rounded_resp_time, current_time))
 
-# Функция для периодической записи данных из очереди в базу данных
+# Function to periodically write queue data to database
 def db_writer():
     conn = sqlite3.connect(config['database']['path'], timeout=30, check_same_thread=False)
     c = conn.cursor()
@@ -205,116 +168,75 @@ def db_writer():
         try:
             conn = sqlite3.connect(config['database']['path'], timeout=30)
             c = conn.cursor()
-            try:
-                # Получаем все данные из очереди
-                while not db_queue.empty():
-                    proxy, rounded_resp_time, current_time = db_queue.get()
-                    c.execute(f'''INSERT OR REPLACE INTO {proxy_type} (proxy, response_time, last_checked) VALUES (?, ?, ?)''', (proxy, rounded_resp_time, current_time))
-                    db_queue.task_done()
-
-                # Записываем накопленные изменения
-                conn.commit()
-            except Exception as e:
-                print(f"Error while writing to database: {e}")
-                conn.rollback()
+            while not db_queue.empty():
+                proxy, rounded_resp_time, current_time = db_queue.get()
+                c.execute(f'''INSERT OR REPLACE INTO {proxy_type} (proxy, response_time, last_checked) VALUES (?, ?, ?)''', (proxy, rounded_resp_time, current_time))
+                db_queue.task_done()
+            conn.commit()
         except Exception as e:
-            print(e)
-            continue
+            print(f"Database write error: {e}")
+            conn.rollback()
         finally:
             conn.close()
             time.sleep(3)
-    
+
+# Function to check proxies in memory
 def check_proxies():
-    # Infinite loop to check proxies stored in memory
     while True:
         try:
-            # If the proxies list is empty, wait for 1 second and continue the loop
             if not proxies:
                 time.sleep(3)
                 continue
-            # Use ThreadPoolExecutor for parallel proxy checking
-            with ThreadPoolExecutor(max_workers=workers/2) as executor:
-                futures = {}
-                # Check each proxy in the proxies list
-                for proxy in random.sample(list(proxies), len(proxies)):
-                    # Submit the task of checking the proxy to the thread pool
-                    future = executor.submit(check_proxy, proxy, proxy_type)
-                    futures[future] = proxy
-                # Iterate over completed futures
+            with ThreadPoolExecutor(max_workers=workers // 2) as executor:
+                futures = {executor.submit(check_proxy, proxy, proxy_type): proxy for proxy in random.sample(list(proxies), len(proxies))}
                 for future in as_completed(futures):
                     result = future.result()
-                    # If the result is not None, the proxy is alive
-                    if result is not None:
+                    if result:
                         proxy, rounded_resp_time, current_time = result
-                        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                         rounded_resp_time = round(rounded_resp_time, 2)
                         alive_proxies_set.add(proxy)
-                        # If the database option is enabled, store the proxy information in the database
                         save_proxy_to_queue(proxy, rounded_resp_time, current_time)
-                    # If the result is None, the proxy is dead and should be removed from the proxies list
                     else:
-                        proxy = futures[future]
-                        proxies.discard(proxy)
+                        proxies.discard(futures[future])
         except:
             pass
-                
+
+# Function to recheck alive proxies periodically
 def recheck_alive_proxies():
     while True:
         try:
-            # Create a copy of the set of alive proxies
             alive_proxies = set(alive_proxies_set)
-            # Use a ThreadPoolExecutor to check all of the alive proxies concurrently using multiple worker threads.
-            with ThreadPoolExecutor(max_workers=workers/2) as executor:
-                futures = {}
-                for proxy in alive_proxies:
-                    future = executor.submit(check_proxy, proxy, proxy_type)
-                    futures[future] = proxy
+            with ThreadPoolExecutor(max_workers=workers // 2) as executor:
+                futures = {executor.submit(check_proxy, proxy, proxy_type): proxy for proxy in alive_proxies}
                 for future in as_completed(futures):
-                    result = future.result()
-                    if result is None:
-                        proxy = futures[future]
-                        # If a proxy check was not successful, remove it from the set of alive proxies in memory.
-                        alive_proxies_set.discard(proxy)
-            # Sleep for 10 seconds before rechecking the proxies again.
+                    if future.result() is None:
+                        alive_proxies_set.discard(futures[future])
             time.sleep(10)
         except:
             pass
 
+# Function to track proxy statistics
 def track_proxies():
     checked_proxies = alive_proxies_set.copy()
-    # For each checked proxy, increment its count in the proxy_stats dictionary. If it is not already present in the dictionary, add it with an initial count of 1.
     for proxy in checked_proxies:
-        if proxy not in proxy_stats:
-            proxy_stats[proxy] = 1
-        else:
-            proxy_stats[proxy] += 1
-        # If the proxy was previously absent, remove it from the proxy_absence_count dictionary.
-        if proxy in proxy_absence_count:
-            del proxy_absence_count[proxy]
-    # Find any proxies that were previously present in the proxy_stats dictionary but are no longer available (checked).
-    absent_proxies = set(proxy_stats.keys()) - checked_proxies
-    # For each absent proxy, increment its count in the proxy_absence_count dictionary. If it is not already present in the dictionary, add it with an initial count of 1.
+        proxy_stats[proxy] = proxy_stats.get(proxy, 0) + 1
+        proxy_absence_count.pop(proxy, None)
+    
+    absent_proxies = set(proxy_stats) - checked_proxies
     for proxy in absent_proxies:
-        if proxy not in proxy_absence_count:
-            proxy_absence_count[proxy] = 1
-        else:
-            proxy_absence_count[proxy] += 1
-        # If a proxy has been absent for more than one iteration, remove it from both the proxy_stats and proxy_absence_count dictionaries.
+        proxy_absence_count[proxy] = proxy_absence_count.get(proxy, 0) + 1
         if proxy_absence_count[proxy] > 0:
             del proxy_stats[proxy]
             del proxy_absence_count[proxy]
-    # Sort the list of proxies by their uptime (count) and get the top 10 proxies.
+    
     top_10_proxies = sorted(proxy_stats.items(), key=lambda x: x[1], reverse=True)[:10]
-    # Create a list of strings to display information about the top 10 proxies by uptime.
     output_strs = [f"\033[1;36mTop {proxy_type} proxies by availability time:\033[0m\n"]
     for i, (proxy, count) in enumerate(top_10_proxies):
         output_strs.append(f"\033[1;33m{i+1}.\033[0m {proxy} \033[32malive for {round(count/6, 2)} min.\033[0m")
     output_strs.append("")
     output_str = "\n".join(output_strs)
     
-    # Clear the screen and print information about the number of proxies in memory and in the last_checked.txt file as well as information about the top 10 proxies by uptime.
     os.system('cls' if os.name == 'nt' else 'clear')
-
     print(f"proxies in memory:\033[1m\033[31m {len(proxies)}\033[0m")
     print(output_str)
 
@@ -390,8 +312,9 @@ def run_thread(func, interval):
             print(e)
             continue
 
+# Main execution
 if __name__ == "__main__":
-    # If the args.db argument is specified, retrieve proxies from the database.
+    
     if args.db:
         conn = sqlite3.connect(config['database']['path'], timeout=30, check_same_thread=False)
         c = conn.cursor()
@@ -399,9 +322,8 @@ if __name__ == "__main__":
         new_proxies = set([row[0] for row in c.fetchall()])
         proxies.update(new_proxies - proxies)
         conn.close()
-        
-    # Очередь для накопления данных
-    db_queue = queue.Queue()
+
+    db_queue = queue.Queue()  
     db_writer_thread = threading.Thread(target=db_writer, daemon=True)
     db_writer_thread.start()
     
@@ -409,7 +331,6 @@ if __name__ == "__main__":
         scan_thread = threading.Thread(target=scan_ip_ranges)
         scan_thread.start()
         
-    # Create and start threads for each of the functions
     t1 = threading.Thread(target=run_thread, args=(get_proxies, 15))
     t2 = threading.Thread(target=check_proxies)
     if not args.api:
